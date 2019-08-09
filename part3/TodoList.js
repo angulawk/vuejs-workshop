ce029c453d905830dff1aa419644e887793f70fd
Sergio Selvaggi <Sergio_Selvaggi@epam.com>
2019-08-09 14:17:37 +0200

    fixed part3

--- part3/TodoList.js
+++ part3/TodoList.js
@@ -1,26 +1,10 @@
 Vue.component('todo-list', {
     data() {
         return {
-            taskInput: "",
-            tasks: [ "Read a book", "Seed a tree" ],
         }
     },
-    props: ['title'],
+    props: [],
     methods: {
-        addTask(event) {
-            event.preventDefault();
-            this.tasks.push(this.taskInput)
-            this.taskInput = ''
-        }
     },
-    template: `<form>
-        <h1>Insert title here</h1>
-        <label for="">New task: </label>
-        <input type="text"
-        v-model="taskInput" 
-        @keydown.enter="addTask($event)">
-        <ul v-for="task in tasks">
-            <li>{{ task }}</li>
-        </ul>
-    </form>`,
+    template: ``,
 })
